b2cda5aada45ae2f85b910af0bafa853ceaab674
Mersikka <mio@argillander.me>
2025-10-29 00:33:50 +0200

    Implement 100% branch coverage for class Varasto

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_neg_alkutilavuus_nollaa(self):
+        self.varasto = Varasto(-10)
+
+        self.assertAlmostEqual(self.varasto.tilavuus, 0.0)
+
+    def test_neg_alku_saldo_nollaa(self):
+        self.varasto = Varasto(10, -10)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_alku_saldo_yli_tilavuus(self):
+        self.varasto = Varasto(10, 15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_lisaa_neg_varastoon_ei_muutu(self):
+        self.varasto.lisaa_varastoon(3)
+        self.varasto.lisaa_varastoon(-2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 7)
+
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(100)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ota_neg(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0.0)
+
+    def test_ota_yli_saldon(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(100), 5.0)
+
+    def test_string_toimii(self):
+        self.varasto.lisaa_varastoon(3)
+
+        self.assertEqual(str(self.varasto), "saldo = 3, vielä tilaa 7")
